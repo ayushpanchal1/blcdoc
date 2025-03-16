19de4f583a7d083aba9aacae48b1dcc6e997c2bf
Ayush Panchal <ayushrockz11@gmail.com>
2025-03-16 20:11:21 +0530

    first commit

---
+++ client/src/UnRegisteredData.js
@@ -0,0 +1,22 @@
+const options = [
+    {
+        id: "verify",
+        name: "Verifier",
+        desc: "Verify authenticity of certificate",
+        imageURL: "/images/verify.png",
+    },
+    {
+        id: "student",
+        name: "User",
+        desc: "View all your certificates uploaded by the organization",
+        imageURL: "/images/user.png",
+    },
+    {
+        id: "issuer",
+        name: "Issuer/Organization",
+        desc: "Issue a New Document",
+        imageURL: "/images/issuer.png",
+    },
+]
+
+export default options;
